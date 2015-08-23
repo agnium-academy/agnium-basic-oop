cef461d77f2ff59a975627b28b63e59217336740
M Haidar Hanif <mhaidarh@users.noreply.github.com>
2015-08-23 15:32:04 +0700

    Some minor changes about naming, title, and format

--- 1 - Introduction to Programming and OOP/BasicsOfProgramming.java
+++ 1 - Introduction to Programming and OOP/BasicsOfProgramming.java
@@ -1,10 +1,19 @@
+/**
+ * Agnium Basics of OOP:
+ * Introduction to Programming and OOP
+ * 
+ * Basics of Programming Example
+ */
+
 public class HelloWorld {
+
   public static void main(String args[]) {
-    //Output using System.out.println
+
+    // Output using System.out.println
+
     System.out.println("Hello, world!");
 
-    /* The following lines will show
-    how to declare variables and assign values to them*/
+    // The following lines will show how to declare variables and assign values to them
 
     int age = 21;
     double tempetature = 98.6;
@@ -13,7 +22,7 @@ public class HelloWorld {
     System.out.println("My name is" + name);
     System.out.println("I am " + age + "years old.");
 
-    //The following lines will show how to perform mathematical operations
+    // The following lines will show how to perform mathematical operations
 
     int a = 11;
     int b = 7;
@@ -22,7 +31,7 @@ public class HelloWorld {
     int product = a * b;
     int remainder = a % b;
 
-    //The following lines show how to perform more decimally accurate math operations
+    // The following lines show how to perform more decimally accurate math operations
 
     double x = 147.65;
     double y = 71;
@@ -32,4 +41,5 @@ public class HelloWorld {
     System.out.println("The difference between " + a + " and " + b + " is " + difference);
 
   }
+
 }
